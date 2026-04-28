e1b4cfd1ac7ba29edc6eaa70401d5d088270fb3c
kaji <dafe00842202@outlook.jp>
2026-04-28 14:13:05 +0900

    update

---
+++ oxgame/oxgame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  oxgame
+//
+//  Created by ip3s on 2026/04/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ oxgame/oxgame/oxgameApp.swift
@@ -0,0 +1,18 @@
+//
+//  oxgameApp.swift
+//  oxgame
+//
+//  Created by ip3s on 2026/04/28.
+//
+
+import SwiftUI
+
+@main
+struct oxgameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
