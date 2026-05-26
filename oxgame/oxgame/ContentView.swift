2307be424b2b5a7fee87526daa3cfd6b02e42454
kaji <dafe00842202@outlook.jp>
2026-05-26 14:01:15 +0900

    update

--- oxgame/oxgame/ContentView.swift
+++ oxgame/oxgame/ContentView.swift
@@ -7,8 +7,34 @@
 
 import SwiftUI
 
+let winPattern: [[Int]] = [
+	[0, 1, 2],
+	[3, 4, 5],
+	[6, 7, 8],
+	[0, 3, 6],
+	[1, 4, 7],
+	[2, 5, 8],
+	[0, 4, 8],
+	[2, 4, 6]
+]
+
 struct ContentView: View {
     var body: some View {
+		@State var boxes: [Bool?] = [
+			nil, nil, nil,
+			nil, nil, nil,
+			nil, nil, nil
+		]
+		
+		func getWinner() -> Bool? {
+			for i in 0..<winPattern.count {
+				if (boxes[winPattern[i][0]] && boxes[winPattern[i][0]] == boxes[winPattern[i][1]] && boxes[winPattern[i][0]] == boxes[winPattern[i][2]]) {
+					return boxes[winPattern[i][0]]
+				}
+				return nil
+			}
+		}
+		
 		VStack {
 			Text("OXGame")
 			VStack {
