3137700a02da763df20521195920f3c26e954f3b
kaji <dafe00842202@outlook.jp>
2026-09-01 13:55:56 +0900

    update

--- cafesns/cafesns/cafesnsApp.swift
+++ cafesns/cafesns/cafesnsApp.swift
@@ -6,10 +6,25 @@
 //
 
 import SwiftUI
+import FirebaseCore
+import FirebaseFirestore
+import FirebaseAuth
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+	func application(_ application: UIApplication,
+					 didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+		FirebaseApp.configure()
+		
+		return true
+	}
+}
+
 
 @main
 struct cafesnsApp: App {
-    var body: some Scene {
+	@UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+	
+	var body: some Scene {
         WindowGroup {
             HomeView()
         }
